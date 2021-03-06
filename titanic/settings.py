6705f960dddca3aac2872a322e0e5c1197388d78
BrentBaker0918 <bakerb_2006@hotmail.com>
2021-03-06 12:31:50 -0500

    trying to get css working

--- titanic/settings.py
+++ titanic/settings.py
@@ -133,6 +133,12 @@ LOGGING = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os.path
+import os
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    # Extra lookup directories for collectstatic to find static files
+)
