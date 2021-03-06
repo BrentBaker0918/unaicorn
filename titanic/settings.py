5cfdbdb3cd8d1a53b5b6b6f43e648a162a97bfb5
BrentBaker0918 <bakerb_2006@hotmail.com>
2021-03-06 10:03:52 -0500

    updated hosts

--- titanic/settings.py
+++ titanic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ogr@f5fxd7xg!n54ss-r1yh5_-o$76b+9)0c-0j@d6tk&e&2$8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000','http://13.59.245.156/']
 
 
 # Application definition
